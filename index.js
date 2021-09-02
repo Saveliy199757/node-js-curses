d899094d02e05355df3ace62f99b3312be5e8674
Saveliy <saveliy199757@gmail.com>
2021-09-02 16:34:48 +0300

    app/part/add-handlebars

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 const app = express();
 
+const hbs = exphbs.create({
+    defaultLayout: "main",
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine); // регистрация движка hbs
+app.set('view engine', 'hbs'); // начала использования hbs
+app.set('views', 'views'); // обозначаем где находятся наши шаблоны
+
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+   res.render('index');
+})
 
 app.listen(PORT, () => {
     console.log("port is run", PORT);
