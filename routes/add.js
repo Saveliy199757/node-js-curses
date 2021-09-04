c95235c60946f8e17ab289f40faa83d2165d9350
Saveliy <saveliy199757@gmail.com>
2021-09-04 09:26:35 +0300

    app/part/list-courses-add

--- routes/add.js
+++ routes/add.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const Courses = require('../models/courses');
 const router = Router();
 
 router.get('/', (req, res) => {
@@ -8,8 +9,10 @@ router.get('/', (req, res) => {
     });
 })
 
-router.post('/', (req, res) => {
-    console.log('data:', req.body)
+router.post('/', async (req, res) => {
+    const courses = new Courses(req.body.title, req.body.price, req.body.img);
+
+    await courses.save();
 
     res.redirect('/courses');
 })
